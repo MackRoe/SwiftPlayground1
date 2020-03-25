4adb9eb97dcac12a738af865e2f91d8f14df3d02
MackRoe <elaine.music@students.makeschool.com>
2020-03-25 10:55:37 -0700

    types of data

--- Contents.swift
+++ Contents.swift
@@ -1,4 +1,18 @@
 import UIKit
 
-var name = "Elaine"
+var name: String
 name = "Juliet"
+
+var age: Int
+age = 52
+
+var latitude: Double
+latitude = 36.166667
+
+var longitude: Double
+longitude = -86.783333
+longitude = -186.783333
+longitude = -1286.783333
+longitude = -12386.783333
+longitude = -123486.783333
+longitude = -1234586.783333
--- README.md
+++
@@ -1,3 +0,0 @@
-# SwiftPlayground1
-
-Repo for tutorial found at [Hacking with Swift](https://www.hackingwithswift.com)
